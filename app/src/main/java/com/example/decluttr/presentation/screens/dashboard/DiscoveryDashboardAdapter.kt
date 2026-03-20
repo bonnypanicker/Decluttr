27fe947f691edfad86539f0a9d652ee1307faed8
bonnypanicker <bonnypanicker@outlook.com>
2026-03-20 23:12:09 +0530

    Fix: Correct import statement syntax error
    - Fixed 'android:widget.EditText' to 'android.widget.EditText'
    - Typo was causing Kotlin compilation error

--- app/src/main/java/com/example/decluttr/presentation/screens/dashboard/DiscoveryDashboardAdapter.kt
+++ app/src/main/java/com/example/decluttr/presentation/screens/dashboard/DiscoveryDashboardAdapter.kt
@@ -10,7 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
-import android:widget.EditText
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
