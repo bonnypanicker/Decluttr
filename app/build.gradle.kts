a96421ed46200d25f9574796dc8ddd015638ffe7
bonnypanicker <bonnypanicker@outlook.com>
2026-04-01 01:12:40 +0530

    Restore Firebase Google Services plugin setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-}
-
-if (file("google-services.json").exists()) {
-    apply(plugin = "com.google.gms.google-services")
 }
 
 android {
