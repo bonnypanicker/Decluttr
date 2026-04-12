ee12a0cd59298628abd845cab1ace12cc374724e
bonnypanicker <bonnypanicker@outlook.com>
2026-04-12 10:25:30 +0530

    Bump fallback app version to 1.0.259

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tool.decluttr"
         minSdk = 26
         targetSdk = 35
-        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 3
-        versionName = System.getenv("VERSION_NAME") ?: "1.0.2"
+        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 259
+        versionName = System.getenv("VERSION_NAME") ?: "1.0.259"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
